35072c2f7449d3f8ad462d06b594075ee71d8c52
Funkoala14 <123384512+Funkoala14@users.noreply.github.com>
2024-11-01 09:42:44 -0400

    [frontend]delete Bus folder

--- bus/src/main.jsx
+++
@@ -1,7 +0,0 @@
-// import "amfe-flexible/index.js";
-import { createRoot } from "react-dom/client";
-import "../tailwind.css";
-import RouterView from "./router/index";
-import "./index.css"
-
-createRoot(document.getElementById("root")).render(<RouterView />);
