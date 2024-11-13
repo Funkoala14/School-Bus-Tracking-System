50c4b36149579dcb83c417dc66a7451728b84d45
susie-laptop <sishi1461@gmail.com>
2024-11-13 17:13:34 -0500

    [frontend]update config

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+// Use environment variables to set the correct baseURL
+const baseURL = import.meta.env.VITE_BACKEND_URL || '/api';
+
 // Create Axios instance with base URL
 const api = axios.create({
-    baseURL: '/api',
+    baseURL,
     withCredentials: true, // Include cookies if needed
 });
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
         port: process.env.VITE_PORT,
         proxy: {
             '/api': process.env.VITE_BACKEND_URL || 'http://localhost:5000', // backend
+            changeOrigin: true,
         },
     },
     define: {
